7f9769f08c3256dfd602e5cbac6a039a46a3faed
inigoromano <inigo.manuelromano@gmail.com>
2025-09-19 09:12:46 +1200

    add fizzbuzz script to demonstrate both for and while loop implementations

--- .idea/fizzbuzz.js
+++ .idea/fizzbuzz.js
@@ -1,3 +1,4 @@
+console.log("Using for loop:")
 for (let i = 1; i <= 15; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz")
@@ -8,4 +9,18 @@ for (let i = 1; i <= 15; i++) {
     } else {
         console.log(i)
     }
+}
+console.log("\nUsing while loop:")
+let count = 1
+while (count <= 15) {
+    if (count % 3 == 0 && count % 5 == 0) {
+        console.log("FizzBuzz")
+    } else if (count % 3 == 0) {
+        console.log("Fizz")
+    } else if (count % 5 == 0) {
+        console.log("Buzz")
+    } else {
+        console.log(count)
+    }
+    count++
 }
