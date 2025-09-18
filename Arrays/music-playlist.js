64ca53365d2bc4302510fb2b4fac2c773c1a1606
inigoromano <inigo.manuelromano@gmail.com>
2025-09-19 00:46:12 +1200

    add music playlist script with initial songs and modifications

---
+++ Arrays/music-playlist.js
@@ -0,0 +1,19 @@
+const musicPlaylist = [
+    "Tom Sawyer",
+    "Sabotage",
+    "I Wanna Dance With Somebody",
+    "Don't Speak",
+    "Bulls On Parade",
+    "Thriller",
+    "The Breaks",
+    "Brick",
+    "Aeroplane Over the Sea",
+    "Tubthumping"
+];
+
+musicPlaylist.shift()
+musicPlaylist.pop()
+musicPlaylist.push("No Sleep Till Brooklyn");
+musicPlaylist.unshift("Brooklyn", "Brooklyn's Finest");
+
+console.log(musicPlaylist);
